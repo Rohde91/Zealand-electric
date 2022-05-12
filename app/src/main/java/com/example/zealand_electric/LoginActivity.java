c721d7ea59606fc0c074405da20f4abcd0c0611b
Marc3724 <marc402m@edu.easj.dk>
2022-05-12 14:25:02 +0200

    arbejdet på activity_main.xml så layout er sat op
    og gjort sådan at login knap sender en videre til activity_main.xml

--- app/src/main/java/com/example/zealand_electric/LoginActivity.java
+++ app/src/main/java/com/example/zealand_electric/LoginActivity.java
@@ -2,15 +2,33 @@ package com.example.zealand_electric;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //Hides toolbar/actionbar
         getSupportActionBar().hide();
+
+        //makes the login button switch scenes
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
