7b17f7b82ee9ca35a522c14cd2ff7a6204ea0f35
alla2297 <alla2297@edu.easj.dk>
2022-05-19 10:20:46 +0200

    has mad a list of checked ja CheckBox's coordinates in ExpandableListViewAdapter.java

--- app/src/main/java/com/example/zealand_electric/ExpandableListViewAdapter.java
+++ app/src/main/java/com/example/zealand_electric/ExpandableListViewAdapter.java
@@ -1,20 +1,27 @@
 package com.example.zealand_electric;
 
 import android.content.Context;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
 
     private Context context;
     private List<String> chapterList; //string type List
     private HashMap<String, List<String>> topicList;   // expandable list is create on hashmap
+    //This  stores the  coordinates  of checkBox's yes: fx 0,1 (group, child)
+    private final Set<Pair<Long, Long>> checkedOn_yes= new HashSet<>();
+
 
     public ExpandableListViewAdapter(Context context, List<String> chapterList, HashMap<String, List<String>> topicList) {
         this.context = context;
@@ -82,7 +89,35 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
         }
         TextView topicsTv = convertView.findViewById(R.id.topics_tv);
         topicsTv.setText(topicsTitle);
+//_____________________________________________________________________
+        final CheckBox cb_yes= (CheckBox) convertView.findViewById(R.id.checkBox1_ja);
+        // add tag to remember groupId/childId
+        final Pair<Long, Long> tag = new Pair<Long, Long>(
+                getGroupId(groupPosition),
+                getChildId(groupPosition, childPosition));
+        cb_yes.setTag(tag);
+        // set checked if groupId/childId in checked items
+        cb_yes.setChecked(checkedOn_yes.contains(tag));
+        // set OnClickListener to handle checked switches
+        cb_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final CheckBox cb = (CheckBox) v;
+                final Pair<Long, Long> tag = (Pair<Long, Long>) v.getTag();
+                if (cb.isChecked()) {
+                        checkedOn_yes.add(tag);
+                } else {
+                    checkedOn_yes.remove(tag);
+                }
+            }
+        });
+//____________________________________________________________
+
         return convertView;
+    }
+
+    public Set<Pair<Long, Long>> getCheckedItems_yes() {
+        return checkedOn_yes;
     }
 
     @Override
