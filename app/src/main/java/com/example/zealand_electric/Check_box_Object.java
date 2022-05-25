89daee85798a82bea2796f0de3b3b84178e021ae
alla2297 <alla2297@edu.easj.dk>
2022-05-25 23:19:35 +0200

    Note funktion work !!! and ready to deploy to datebaset need just database Funktion to sned it to

--- app/src/main/java/com/example/zealand_electric/Check_box_Object.java
+++ app/src/main/java/com/example/zealand_electric/Check_box_Object.java
@@ -4,11 +4,38 @@ public class Check_box_Object {
     private int category_position;
     private int question_position;
     private int answerOnCheckBox;
+    private String note;
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Check_box_Object(int category_position,int question_position, String note) {
+        this.category_position = category_position;
+        this.question_position = question_position;
+        this.note = note;
+    }
+
+    public Check_box_Object(int category_position, int question_position) {
+        this.category_position = category_position;
+        this.question_position = question_position;
+    }
 
     public Check_box_Object(int category_position, int question_position, int answerOnCheckBox) {
         this.category_position = category_position;
         this.question_position = question_position;
         this.answerOnCheckBox = answerOnCheckBox;
+    }
+
+    public Check_box_Object(int category_position, int question_position, int answerOnCheckBox, String note) {
+        this.category_position = category_position;
+        this.question_position = question_position;
+        this.answerOnCheckBox = answerOnCheckBox;
+        this.note = note;
     }
 
     public int getCategory_position() {
@@ -33,5 +60,14 @@ public class Check_box_Object {
 
     public void setAnswerOnCheckBox(int answerOnCheckBox) {
         this.answerOnCheckBox = answerOnCheckBox;
+    }
+
+    @Override
+    public String toString() {
+        return "Check_box_Object{" +
+                "category_position=" + category_position +
+                ", question_position=" + question_position +
+                ", note='" + note + '\'' +
+                '}';
     }
 }
