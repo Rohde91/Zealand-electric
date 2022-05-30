9a182658f481d4cd87d4ae4c8a9ae02dbd4f07a4
Marc3724 <marc402m@edu.easj.dk>
2022-05-30 09:23:19 +0200

    Hele knapperne virker nu på result_tabs2, skulle "bare" flytte lidt på viewpager

---
+++ app/src/test/java/com/example/zealand_electric/DBControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.zealand_electric;
+
+import junit.framework.TestCase;
+
+public class DBControllerTest extends TestCase {
+
+    public void testConnectToDatabase() {
+
+    }
+}
